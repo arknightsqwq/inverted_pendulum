3384e65fb8e0294f451e1716031f038ca4c5f927
asus <3048882087@qq.com>
2026-05-07 01:39:38 +0800

    rewrite using cpp

--- User/Src/Interrupt.cpp
+++ User/Src/Interrupt.cpp
@@ -27,7 +27,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
  */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
     if (GPIO_Pin == GPIO_PIN_0) {
-        pc_link.Send("External Interrupt Triggered!\r\n");
+        pclink.Send("External Interrupt Triggered!\r\n");
     }
 }
 
